f3d997c40e1ac25bde6780c873284fecaba60ed8
Naheel Palliyali <naheel.palliyali@gmail.com>
2022-01-23 16:47:20 +0530

    Prepared for deployment

--- backend/server.js
+++ backend/server.js
@@ -19,10 +19,6 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'))
 }
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
-
 app.use(express.json())
 
 app.use('/api/products', productRoutes)
@@ -36,6 +32,18 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
