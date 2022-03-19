235545b61a7c237482c327b3dbf5ee3d07238a6f
Naheel Palliyali <naheel.palliyali@gmail.com>
2022-03-19 23:11:35 +0530

    changed carousel css

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -9,39 +9,50 @@ import { PRODUCT_DETAILS_REQUEST } from '../constants/productConstants'
 import Rating from '../components/Rating'
 
 const ProductCarousel = () => {
-  const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-  const topProducts = useSelector((state) => state.topProducts)
-  const { loading, error, products } = topProducts
+    const topProducts = useSelector((state) => state.topProducts)
+    const { loading, error, products } = topProducts
 
-  useEffect(() => {
-    dispatch(getTopProducts())
-  }, [dispatch])
+    useEffect(() => {
+        dispatch(getTopProducts())
+    }, [dispatch])
 
-  const clickProductHandler = () => {
-    dispatch({ type: PRODUCT_DETAILS_REQUEST })
-  }
+    const clickProductHandler = () => {
+        dispatch({ type: PRODUCT_DETAILS_REQUEST })
+    }
 
-  return loading ? (
-    <Loader />
-  ) : error ? (
-    <Message variant='danger' message={error} />
-  ) : (
-    <Carousel pause='hover' className='d-block'>
-      {products.map((item) => (
-        <Carousel.Item onClick={clickProductHandler}>
-          <Link to={`/product/${item._id}`}>
-            <Image className='d-block' src={item.image} alt={item.name} fluid />
-            <Carousel.Caption>
-              <h3>{item.name}</h3>
-              <p>${item.price}</p>
-              <Rating value={item.rating} />
-            </Carousel.Caption>
-          </Link>
-        </Carousel.Item>
-      ))}
-    </Carousel>
-  )
+    return loading ? (
+        <Loader />
+    ) : error ? (
+        <Message variant='danger' message={error} />
+    ) : (
+        <Carousel pause='hover' className='d-block'>
+            {products.map((item) => (
+                <Carousel.Item onClick={clickProductHandler}>
+                    <Link to={`/product/${item._id}`}>
+                        <div className='carousel-content-wrapper'>
+                            <div className='image-container'>
+                                <Image
+                                    className='d-block w-100'
+                                    src={item.image}
+                                    alt={item.name}
+                                    fluid
+                                />
+                            </div>
+                            <div className='text-container'>
+                                <h3>{item.name}</h3>
+                                {/* <p>${item.price}</p> */}
+                                <div className='carousel-rating'>
+                                    <Rating value={item.rating} />
+                                </div>
+                            </div>
+                        </div>
+                    </Link>
+                </Carousel.Item>
+            ))}
+        </Carousel>
+    )
 }
 
 export default ProductCarousel
