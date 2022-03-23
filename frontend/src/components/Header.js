cf0d8712cedad0779af795353d1e074c7ad719f0
Naheel Palliyali <naheel.palliyali@gmail.com>
2022-03-24 00:41:26 +0530

    added save point

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -16,83 +16,92 @@ const cart = <FontAwesomeIcon icon={faShoppingCart} />
 const user = <FontAwesomeIcon icon={faUser} />
 
 const Header = () => {
-  const dispatch = useDispatch()
+    const dispatch = useDispatch()
 
-  const userLogin = useSelector((state) => state.userLogin)
-  const { userInfo } = userLogin
+    const userLogin = useSelector((state) => state.userLogin)
+    const { userInfo } = userLogin
 
-  const logoutHandler = () => {
-    dispatch({ type: USER_DETAILS_RESET })
-    dispatch({ type: MY_ORDERS_RESET })
-    dispatch(logout())
-  }
+    const logoutHandler = () => {
+        dispatch({ type: USER_DETAILS_RESET })
+        dispatch({ type: MY_ORDERS_RESET })
+        dispatch(logout())
+    }
 
-  return (
-    <header>
-      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
-        <Container>
-          <Link to='/' style={{ textDecoration: 'none' }}>
-            <Navbar.Brand>Shop</Navbar.Brand>
-          </Link>
-          <Navbar.Toggle aria-controls='navbarScroll' />
-          <Navbar.Collapse id='navbarScroll'>
-            <SearchBox />
-            <Nav style={{ marginLeft: 'auto' }} navbarScroll>
-              <Nav.Link
-                href='/cart'
-                style={{
-                  textDecoration: 'none',
-                  color: 'white',
-                  opacity: 0.6,
-                }}
-              >
-                {cart} Cart
-              </Nav.Link>
+    return (
+        <header>
+            <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+                <Container>
+                    <Link to='/' style={{ textDecoration: 'none' }}>
+                        <Navbar.Brand>Shop</Navbar.Brand>
+                    </Link>
+                    <Navbar.Toggle aria-controls='navbarScroll' />
+                    <Navbar.Collapse id='navbarScroll'>
+                        <SearchBox />
+                        <Nav style={{ marginLeft: 'auto' }} navbarScroll>
+                            <Nav.Link
+                                href='/cart'
+                                style={{
+                                    textDecoration: 'none',
+                                    color: 'white',
+                                    opacity: 0.6,
+                                }}
+                            >
+                                {cart} Cart
+                            </Nav.Link>
 
-              {userInfo ? (
-                <NavDropdown title={userInfo.name} id='username'>
-                  <NavDropdown.Item>
-                    <Link to='/profile'>Profile</Link>
-                  </NavDropdown.Item>
-                  {userInfo.isAdmin && (
-                    <NavDropdown.Item>
-                      <Link to='/api/admin/users'>Manage users</Link>
-                    </NavDropdown.Item>
-                  )}
-                  {userInfo.isAdmin && (
-                    <NavDropdown.Item>
-                      <Link to='/api/admin/orders'>Manage orders</Link>
-                    </NavDropdown.Item>
-                  )}
-                  {userInfo.isAdmin && (
-                    <NavDropdown.Item>
-                      <Link to='/api/admin/products'>Manage products</Link>
-                    </NavDropdown.Item>
-                  )}
+                            {userInfo ? (
+                                <NavDropdown
+                                    title={userInfo.name}
+                                    id='username'
+                                >
+                                    <NavDropdown.Item>
+                                        <Link to='/profile'>Profile</Link>
+                                    </NavDropdown.Item>
+                                    {userInfo.isAdmin && (
+                                        <NavDropdown.Item>
+                                            <Link to='/api/admin/users'>
+                                                Manage users
+                                            </Link>
+                                        </NavDropdown.Item>
+                                    )}
+                                    {userInfo.isAdmin && (
+                                        <NavDropdown.Item>
+                                            <Link to='/api/admin/orders'>
+                                                Manage orders
+                                            </Link>
+                                        </NavDropdown.Item>
+                                    )}
+                                    {userInfo.isAdmin && (
+                                        <NavDropdown.Item>
+                                            <Link to='/api/admin/products'>
+                                                Manage products
+                                            </Link>
+                                        </NavDropdown.Item>
+                                    )}
 
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
-                </NavDropdown>
-              ) : (
-                <Nav.Link
-                  href='/login'
-                  style={{
-                    textDecoration: 'none',
-                    color: 'white',
-                    opacity: 0.6,
-                    marginLeft: '24px',
-                  }}
-                >
-                  {user} Sign in
-                </Nav.Link>
-              )}
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
-    </header>
-  )
+                                    <NavDropdown.Item onClick={logoutHandler}>
+                                        Logout
+                                    </NavDropdown.Item>
+                                </NavDropdown>
+                            ) : (
+                                <Nav.Link
+                                    href='/login'
+                                    style={{
+                                        textDecoration: 'none',
+                                        color: 'white',
+                                        opacity: 0.6,
+                                        // marginLeft: '24px',
+                                    }}
+                                >
+                                    {user} Sign in
+                                </Nav.Link>
+                            )}
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
 }
 
 export default Header
